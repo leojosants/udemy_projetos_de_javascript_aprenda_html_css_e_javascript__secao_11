38e0e6dd6f9e6e14e1210d61f707dc6e71b80bdd
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-28 00:54:36 -0300

    class 43 - creating the cards

--- jogo_da_memoria/assets/js/app.js
+++ jogo_da_memoria/assets/js/app.js
@@ -23,12 +23,36 @@ function shuffleCards(cards) {
 async function createCards() {
     const imagePairs = await generateImagePairs();
     shuffleCards(cards);
-    console.log(cards);
+    const cardsList = document.querySelector('[data_container]');
+
+    for (let i = 0; i < cards.length; i++) {
+        const card = document.createElement('div');
+        const cardBack = document.createElement('div');
+        const cardFront = document.createElement('div');
+
+        card.classList.add('card');
+        cardBack.classList.add('back');
+        cardFront.classList.add('front');
+        cardBack.style.backgroundImage = `url('assets/img/card_back.png')`;
+
+        const cardNumber = cards[i];
+        const cardImage = imagePairs[cardNumber].pop();
+
+        cardFront.style.backgroundImage = `url(${cardImage})`;
+        card.setAttribute('data_card', cardNumber);
+        card.appendChild(cardBack);
+        card.appendChild(cardFront);
+        card.addEventListener('click', flipCard);
+        cardsList.appendChild(card);
+    };
+};
+
+function flipCard() { 
+    console.log('virou');
 };
 
 createCards();
 
 // ('[data_congratulations_container]');
-// ('[data_container]');
 // ('[data_attempts_container]');
 // ('[data_attempts]');
